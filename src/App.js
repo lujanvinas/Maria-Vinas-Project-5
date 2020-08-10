fb3f8cb3992151773919902e74222c79d6b11ad9
Maria <lu_vinas@hotmail.com>
2020-08-10 17:05:31 -0400

    setting up REACT project :rocket:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import firebase from './firebase.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      items: [],
+      currentItem: {
+        text:'',
+        key: ''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+
+  }
+
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+
+  addItem(e){
+    e.preventDefault();
+    const newItem= this.state.currentItem;
+    console.log(newItem);
+    if(newItem.text!==""){
+      const newItems=[...this.state.items, newItems];
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text: '',
+          key:''
+        }
+      })
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Packing List</h1>
+        <h2>Going on a trip? Add items here:</h2>
+        <form id="packing-list" onSubmit={this.addItem}>
+          <input type="text" placeholder="Enter Item" value= {this.state.currentItem.text} onChange={this.handleInput}/>
+          <button type="submit">Add item</button>
+        </form>
+        <ul>
+        {/* {
+          this.state.books.map((book) => {
+            return <li>{book}</li>
+          })      
+          
+        } */}
+        </ul>
+        
+      </div>
+    );
+  }
+  
+
+
+  }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDZchN1Zm4hZekdaZ00bZcsPw60gGhzOUs",
+    authDomain: "packing-list-cc226.firebaseapp.com",
+    databaseURL: "https://packing-list-cc226.firebaseio.com",
+    projectId: "packing-list-cc226",
+    storageBucket: "packing-list-cc226.appspot.com",
+    messagingSenderId: "624836951041",
+    appId: "1:624836951041:web:3a5b1b98ba30fb431f5400"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
