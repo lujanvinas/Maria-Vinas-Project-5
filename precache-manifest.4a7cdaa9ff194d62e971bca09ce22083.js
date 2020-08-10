b8e1a018f1e3d3bd054bb8f7ed4134cdd5733f06
Maria <lu_vinas@hotmail.com>
2020-08-10 16:41:56 -0400

    Updates

---
+++ precache-manifest.4a7cdaa9ff194d62e971bca09ce22083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0c7154575166ad17497143de61b049",
+    "url": "/Maria-Vinas-Project-5/index.html"
+  },
+  {
+    "revision": "8e7101238105cc61f292",
+    "url": "/Maria-Vinas-Project-5/static/css/main.a4fb5bd1.chunk.css"
+  },
+  {
+    "revision": "ef2f276811ab3bdeaf55",
+    "url": "/Maria-Vinas-Project-5/static/js/2.b469f129.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/Maria-Vinas-Project-5/static/js/2.b469f129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e7101238105cc61f292",
+    "url": "/Maria-Vinas-Project-5/static/js/main.670022bd.chunk.js"
+  },
+  {
+    "revision": "746e475d0b5a27e4363f",
+    "url": "/Maria-Vinas-Project-5/static/js/runtime-main.a1594eb1.js"
+  }
+]);
