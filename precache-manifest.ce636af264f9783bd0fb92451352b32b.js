f534b5def22f6894a59d351ac3163f84cc2235db
Maria <lu_vinas@hotmail.com>
2020-08-14 09:47:38 -0400

    Updates

--- precache-manifest.ce636af264f9783bd0fb92451352b32b.js
+++ precache-manifest.ce636af264f9783bd0fb92451352b32b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17796aeaeec0c127b6272588141a2212",
+    "revision": "11c202511d49da9d22b5e2257197599b",
     "url": "/Maria-Vinas-Project-5/index.html"
   },
   {
-    "revision": "bec487786f46abb9453e",
-    "url": "/Maria-Vinas-Project-5/static/css/main.2cac60ac.chunk.css"
+    "revision": "db09fe330260adf30c8f",
+    "url": "/Maria-Vinas-Project-5/static/css/main.abb00811.chunk.css"
   },
   {
     "revision": "7c009255de73e5fac922",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Maria-Vinas-Project-5/static/js/2.278e738b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bec487786f46abb9453e",
-    "url": "/Maria-Vinas-Project-5/static/js/main.65634622.chunk.js"
+    "revision": "db09fe330260adf30c8f",
+    "url": "/Maria-Vinas-Project-5/static/js/main.25fc954b.chunk.js"
   },
   {
     "revision": "746e475d0b5a27e4363f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Maria-Vinas-Project-5/precache-manifest.5c33363fbe69886c5a333de984dc66dc.js"
+  "/Maria-Vinas-Project-5/precache-manifest.ce636af264f9783bd0fb92451352b32b.js"
 );
 
 self.addEventListener('message', (event) => {
